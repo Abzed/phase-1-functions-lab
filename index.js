ac0f14bbd4c56503d805f81e27c1a808a7fdba8e
Abzed <ibraabzed@gmail.com>
2022-03-21 18:01:08 +0300

    commit tree 1440e4d74f68e3d0ada617229b90c5acdd30ef83

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num1) {
+    const num2 = 42;
+    return Math.abs(num1 - num2)
+}
+
+
+
+function distanceFromHqInFeet(num3) {
+    let block = distanceFromHqInBlocks(num3)
+    return block * 264
+}
+
+
+
+function distanceTravelledInFeet(num4, num5) {
+    return Math.abs(distanceFromHqInFeet(num4) - distanceFromHqInFeet(num5))
+}
+
+
+
+function calculatesFarePrice(num6, num7) {
+    let price = distanceTravelledInFeet(num6, num7);
+    if (price <= 400) {
+        return 0
+    }
+    else if(price > 400 && price <= 2000){
+        return (price - 400) * 0.02;
+    }
+    else if(price > 2000 && price <= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
+
+
+
+
+
+
